bd396690b7a054950c2e84d7290533daadb9fe4c
Epiphany6666 <115459047+Epiphany6666@users.noreply.github.com>
2023-06-11 21:55:21 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -6,30 +6,11 @@ int main()
 	int b = 0;
 	int c = 0;
 	int d = 0;
-	int e = 0;
-	for (a = 1; a <= 5; a++)
+	int killer = 0;
+	for (killer = 'a'; killer <= 'd'; killer++)
 	{
-		for (b = 1; b <= 5; b++)
-		{
-			for (c = 1; c <= 5; c++)
-			{
-				for (d = 1; d <= 5; d++)
-				{
-					for (e = 1; e <= 5; e++)
-					{
-						if ((((b == 2) + (a == 3)) == 1)
-							&& (((b == 2) + (e == 4)) == 1)
-							&& (((c == 1) + (d == 2)) == 1)
-							&& (((c == 5) + (d == 3)) == 1)
-							&& (((e == 4) + (a == 1)) == 1))
-						{
-							if (a * b * c * d * e == 120)
-								printf("%d %d %d %d %d\n", a, b, c, d, e);
-						}
-					}
-				}
-			}
-		}
+		if ((killer != 'a') + (killer == 'c') + (killer == 'd') + (killer != 'd') == 3)
+			printf("%c\n", killer);
 	}
 	return 0;
 }
